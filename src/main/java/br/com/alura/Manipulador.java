f67868ceb4094f7e8b9420860d7f6c5271f838c3
darpbr <dipeixoto@gmail.com>
2022-07-18 17:37:03 -0300

    ajustes token

--- src/main/java/br/com/alura/Manipulador.java
+++ src/main/java/br/com/alura/Manipulador.java
@@ -1,4 +1,18 @@
 package br.com.alura;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+// Classe que vai manipular o token para que não fique visível no repositório publico
 public class Manipulador {
+
+//    Retorna o arquivo de propriedades
+    public static Properties getProp() throws IOException {
+        Properties props = new Properties();
+        FileInputStream file = new FileInputStream("./src/main/resources/dados.properties");
+        props.load(file);
+        return props;
+
+    }
 }
--- src/main/java/br/com/alura/Principal.java
+++ src/main/java/br/com/alura/Principal.java
@@ -7,13 +7,18 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+
+import static br.com.alura.Manipulador.getProp;
 
 public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
-//        System.out.println("Hello World!");
 
-        String tokenIMDB = "k_frojmf22";
+//        Propriedades do token
+        Properties prop = getProp();
+        String tokenIMDB = prop.getProperty("prop.token");
         String url = "https://imdb-api.com/en/API/Top250Movies/";
+        String urlFilmesPopulares = "https://imdb-api.com/en/API/MostPopularMovies/";
 
 //         Fazer conexão HTTP para buscar os TOP 250 Filmes (JSON)
         URI endereco = URI.create(url+tokenIMDB);
@@ -21,7 +26,6 @@ public class Principal {
         HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         String json = response.body();
-//        System.out.println(json);
 
 //        fazer o parser do JSON com os dados que nos interessam - Título, poster (imagem) e
 //        classificação (rating)
@@ -35,7 +39,12 @@ public class Principal {
 
 //        Exibir e manipular os dados
         for (Map<String, String> filme: listaFilmes ) {
-            System.out.println(filme.get("title"));
+            System.out.println("\u001b[1m \u001b[41m Titulo: \u001b[m " +
+                    filme.get("title"));
+            System.out.println("\u001b[1m \u001b[41m Poster: \u001b[m " +
+                    filme.get("image"));
+            System.out.println("\u001b[1m \u001b[41m Nota: \u001b[m " +
+                    filme.get("imDbRating"));
         }
     }
 }
