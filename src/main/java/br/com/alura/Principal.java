cb389ee138e137daf307902a28396b2b167f8f8c
darpbr <dipeixoto@gmail.com>
2022-07-20 12:44:41 -0300

    Desafio 1

--- src/main/java/br/com/alura/Principal.java
+++ src/main/java/br/com/alura/Principal.java
@@ -21,26 +21,27 @@ public class Principal {
         String urlTopFilmes = "https://imdb-api.com/en/API/Top250Movies/";
         String urlFilmesPopulares = "https://imdb-api.com/en/API/MostPopularMovies/";
         String urlMockito = "https://api.mocki.io/v2/549a5d8b/Top250Movies";
-        String urlTeste = "https://api.nasa.gov/planetary/apod?api_key=wfEP3Sup3sUNZKOWuZfIZ9xy98nQkrtbtSddgbq4&start_date=2022-07-19&end_date=2022-07-20";
+        String urlTeste = urlNasa+tokenNasa+"&count=3";
 
-        String json = new ClienteHttp().getDadosSemToken(urlMockito);
-        List<Conteudo> listaConteudos = new ExtratorConteudoImdb().extraiConteudo(json);
+//        String json = new ClienteHttp().getDadosSemToken(urlMockito);
+//        List<Conteudo> listaConteudos = new ExtratorConteudoImdb().extraiConteudo(json);
 
-//        String json = new ClienteHttp().getDadosSemToken(urlTeste);
-//        List<Conteudo> listaConteudos = new ExtratorConteudoNasa().extraiConteudo(json);
+        String json = new ClienteHttp().getDadosSemToken(urlTeste);
+        System.out.println(json);
+        List<Conteudo> listaConteudos = new ExtratorConteudoNasa().extraiConteudo(json);
 
         GeradoraDeSticker geradora = new GeradoraDeSticker();
 
 //        Exibir e manipular os dados
         for (Conteudo conteudo: listaConteudos ) {
             Double notaFilme = 10.0;
-            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            InputStream inputStream = new URL(conteudo.urlImagem()).openStream();
             System.out.println("\u001b[1m \u001b[41m Titulo: \u001b[m " +
-                    conteudo.getTitulo());
+                    conteudo.titulo());
             System.out.println("\u001b[1m \u001b[44m Poster: \u001b[m " +
-                    conteudo.getUrlImagem());
+                    conteudo.urlImagem());
 
-            geradora.cria(inputStream, conteudo.getTitulo()+".png", notaFilme);
+            geradora.cria(inputStream, conteudo.titulo()+".png", notaFilme);
         }
         System.out.println("Tamanho do arquivo: " + listaConteudos.size());
     }
